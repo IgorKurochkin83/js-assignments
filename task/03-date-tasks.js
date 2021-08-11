3756719c199d1077d302602c771d389771d27944
Igor Kurochkin <igor_k_99@mail.ru>
2021-08-11 15:16:21 +0300

    section 3

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,14 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let answer  = false;
+   let year    = date.getFullYear();
+
+   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      answer = true;
+   }
+
+   return answer;
 }
 
 
@@ -76,7 +83,21 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let outputStr = 'HH:mm:ss.sss';
+
+   let time = startDate.getTime() - endDate.getTime();
+
+   outputStr = outputStr.replace('sss', String(time % 1000).padStart(3, '0')).replace('ss', String(Math.trunc((time % 100000) / 1000)).padStart(2, '0'));
+   
+   time = Math.trunc(time / 100000);
+
+   outputStr = outputStr.replace('mm', String(time % 60).padStart(2, '0'));
+
+   time = Math.trunc(time / 60);
+
+   outputStr = outputStr.replace('HH', String(time % 60).padStart(2, '0'));
+ 
+   return outputStr;
 }
 
 
@@ -94,7 +115,11 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   let angle;
+   
+   angle = (Math.PI / 180) * Math.abs(0.5 * (60 * date.getHours() + date.getMinutes()) - 6 * date.getMinutes());
+
+   return angle <= Math.PI ? angle : (2 * Math.PI - angle);
 }
 
 
